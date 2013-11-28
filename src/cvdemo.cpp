9a9f3476ded76011f82afc33ead966a931bfab34
gustavxerxes <mrtnloof@gmail.com>
2013-11-28 23:24:06 +0100

    fått igång z, väldigt skakig, får fel på magin

--- src/cvdemo.cpp
+++ src/cvdemo.cpp
@@ -3,98 +3,86 @@
 #include <math.h>
 #include <stdio.h>
 #include "libfreenect_cv.h"
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 
 
+//#define ADJ_GAMMA
+#define GAMMA_EXPONENT 2.0
 
-IplImage *GlViewColor(IplImage *depth)
-{
-	static IplImage *image = 0;
-	if (!image) image = cvCreateImage(cvSize(640,480), 8, 1);
-	/*
-	unsigned char *depth_mid = (unsigned char*)(image->imageData);
-	int i;
-	for (i = 0; i < 640*480; i++) {
-		int lb = ((short *)depth->imageData)[i] % 256;
-		int ub = ((short *)depth->imageData)[i] / 256;
-		switch (ub) {
-			case 0:
-				depth_mid[3*i+2] = 255;
-				depth_mid[3*i+1] = 255-lb;
-				depth_mid[3*i+0] = 255-lb;
-				break;
-			case 1:
-				depth_mid[3*i+2] = 255;
-				depth_mid[3*i+1] = lb;
-				depth_mid[3*i+0] = 0;
-				break;
-			case 2:
-				depth_mid[3*i+2] = 255-lb;
-				depth_mid[3*i+1] = 255;
-				depth_mid[3*i+0] = 0;
-				break;
-			case 3:
-				depth_mid[3*i+2] = 0;
-				depth_mid[3*i+1] = 255;
-				depth_mid[3*i+0] = lb;
-				break;
-			case 4:
-				depth_mid[3*i+2] = 0;
-				depth_mid[3*i+1] = 255-lb;
-				depth_mid[3*i+0] = 255;
-				break;
-			case 5:
-				depth_mid[3*i+2] = 0;
-				depth_mid[3*i+1] = 0;
-				depth_mid[3*i+0] = 255-lb;
-				break;
-			default:
-				depth_mid[3*i+2] = 0;
-				depth_mid[3*i+1] = 0;
-				depth_mid[3*i+0] = 0;
-				break;
-		}
-	}*/
-	return image;
-}
+using namespace cv;
+using namespace std;
+
 
 int main(int argc, char **argv)
 {
 
+#ifdef ADJ_GAMMA
 	unsigned char gamma[256];
-
 	for(int i = 0; i < 256; i++){
-				gamma[i] =
-				gamma[i] = (char) pow((float)i/255.0, 4.0)*255;
+
+				if(i < 100){
+					gamma[i] = 0;
+				} else {
+
+				gamma[i] = (char) round( pow((float)(2*(i-127.5))/255.0, GAMMA_EXPONENT)*255);
+				}
+
 			}
+#endif
+
+	int x = 0;
+	int y = 0;
+	int z = 0;
+
 
 	while (cvWaitKey(10) < 0) {
-		IplImage *image = freenect_sync_get_rgb_cv(0);
+		Mat image = freenect_sync_get_rgb_cv(0);
+
+		/*
 		if (!image) {
 		    printf("Error: Kinect not connected?\n");
 		    return -1;
 		}
+		*/
 
-		/*
-		cvCvtColor(image, image, CV_RGB2BGR);
-		IplImage *depth = freenect_sync_get_depth_cv(0);
-		if (!depth) {
-		    printf("Error: Kinect not connected?\n");
-		    return -1;
-		}
-
-		//cvShowImage("Depth", GlViewColor(depth));
-
-
-
-		//cvErode(image, image, 0, 1);
-		 * *
-		 */
-		unsigned char *a = (unsigned char*) image->imageData;
+	    GaussianBlur(image,image, Size(9,9), 7,7);
+#ifdef ADJ_GAMMA
+		unsigned char *a = (unsigned char*) image.data;
 		for(int i = 0; i < 307200; i++){
+
 			a[i] = gamma[a[i]];
 		}
+#endif
 
-		cvShowImage("IR", image);
+	    vector<Vec3f> circles;
+
+	   /// Apply the Hough Transform to find the circles
+
+	    HoughCircles( image, circles, CV_HOUGH_GRADIENT, 1 ,400, 200, 1, 15, 100 );
+
+	    if(circles.size() != 0){
+	    x = circles[0][0];
+	    y = circles[0][1];
+	    IplImage *depth = freenect_sync_get_depth_cv(0);
+	    z = depth->imageData[x*1280+y*2];
+	    }
+
+
+	    printf("x %d y %d z %d\n", x, y, z);
+	   /// Draw the circles detected
+	    for( size_t i = 0; i < circles.size(); i++ )
+	    {
+
+	         Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
+	         int radius = cvRound(circles[i][2]);
+	         // circle center
+	         circle( image, center, 3, Scalar(255,0,0), -1, 8, 0 );
+	         // circle outline
+	         circle( image, center, radius, Scalar(0,0,255), 3, 8, 0 );
+	    }
+
+		imshow("IR", image);
 	}
 	return 0;
 }
